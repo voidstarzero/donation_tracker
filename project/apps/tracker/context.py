795b55b5c88d8452f17f8ec2a9a02f62adc2af44
James Arcus <jimbo@ucc.asn.au>
2022-02-06 14:04:19 +0800

    Let total message change based on amount

--- project/apps/tracker/context.py
+++ project/apps/tracker/context.py
@@ -1,8 +1,21 @@
+from os import environ
+
 from django.db.models import Sum
 
 from .models import Event
 
+def raised_message(total):
+    if total < 20:
+        return "Let's go!"
+    elif total < 100:
+        return "Keep up the good work!"
+    else:
+        return "Well done!"
+
 def statistics(request):
+    raised_total = Event.objects.aggregate(Sum('balance__balance'))['balance__balance__sum'] or 0
+
     return {
-        'raised_total': Event.objects.aggregate(Sum('balance__balance'))['balance__balance__sum'],
+        'raised_total': raised_total,
+        'raised_message': raised_message(raised_total),
     }
